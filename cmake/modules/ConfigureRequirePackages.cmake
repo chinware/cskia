cea6a6540f1b427b8a3c35b12d322c6634098af0
qinware <qinware@163.com>
2025-08-06 00:44:00 +0800

    Add fetch GN

--- cmake/modules/ConfigureRequirePackages.cmake
+++ cmake/modules/ConfigureRequirePackages.cmake
@@ -7,3 +7,18 @@
 
 find_package(Git REQUIRED)
 find_package(Python3 REQUIRED)
+
+if(NOT EXISTS "${CSKIA_SKIA_GN_EXECUTABLE}")
+    execute_process(
+        COMMAND ${Python3_EXECUTABLE} fetch-gn
+        WORKING_DIRECTORY ${CSKIA_SKIA_BIN_DIR}
+        RESULT_VARIABLE CSKIA_FETCH_GN_AVAILABLE
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(${CSKIA_FETCH_GN_AVAILABLE} EQUAL 0)
+        message(STATUS "Fetch GN successfully")
+    else()
+        message(STATUS "Fetch GN failed")
+    endif()
+endif()
+
--- cmake/modules/ImportSkia.cmake
+++ cmake/modules/ImportSkia.cmake
@@ -5,3 +5,12 @@
 #
 # Created by CHINBOY on 2025/08/03.
 
+set(CSKIA_SKIA_SOURCE_DIR "${CSKIA_THIRDPARTY_DIR}/skia")
+set(CSKIA_SKIA_BIN_DIR "${CSKIA_SKIA_SOURCE_DIR}/bin")
+set(CSKIA_SKIA_GN_EXECUTABLE "${CSKIA_SKIA_SOURCE_DIR}/bin/gn")
+set(CSKIA_SKIA_BUILD_DIR "${CSKIA_BINARY_DIR}/skia-build")
+set(CSKIA_SKIA_INSTALL_DIR "${CSKIA_BINARY_DIR}/skia-install")
+
+macro(inport_skia)
+
+endmacro()
